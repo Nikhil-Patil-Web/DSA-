43a3f3c90df520745c9f433f8397de33da19e03f
Nikhil Patil <84954050+Nikhil-Patil-Web@users.noreply.github.com>
2023-03-02 22:55:36 +0530

    Create StringCompression.cpp

---
+++ Leetcode/StringCompression.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        if(n == 1) return 1;
+
+        int cnt=1, i=0, j=0;
+
+        while(i < n){
+            while(i<n-1 and chars[i] == chars[i+1]) 
+                cnt++, i++;
+
+            chars[j++] = chars[i++];
+            if(cnt > 1){
+                string len=to_string(cnt);
+                for(auto c : len)
+                    chars[j++] = c;
+                cnt=1;
+            }
+        }
+
+        return j;
+    }
+};
